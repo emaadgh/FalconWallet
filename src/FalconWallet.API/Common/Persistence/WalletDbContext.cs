eb9557fa87a92da46f10bbdcebc81e4af3ea9c92
emaadghorbani <emaadghorbani@gmail.com>
2024-07-01 10:48:43 +0330

    - Adds project's dbcontext. - Adds EF Core dependencies to the project.

---
+++ src/FalconWallet.API/Common/Persistence/WalletDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FalconWallet.API.Features.MultiCurrency;
+using FalconWallet.API.Features.UserWallet.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FalconWallet.API.Common.Persistence;
+
+public class WalletDbContext : DbContext
+{
+    public WalletDbContext(DbContextOptions<WalletDbContext> dbContextOptions)
+        : base(dbContextOptions)
+    {
+
+    }
+
+    public DbSet<Wallet> Wallets => Set<Wallet>();
+    public DbSet<Currency> Currency => Set<Currency>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
