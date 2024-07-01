1965a430896927d8440b8634a4633d7282fe640a
emaadghorbani <emaadghorbani@gmail.com>
2024-07-01 10:47:30 +0330

    Adds domain models.

---
+++ src/FalconWallet.API/Features/MultiCurrency/Currency.cs
@@ -0,0 +1,11 @@
+﻿namespace FalconWallet.API.Features.MultiCurrency;
+
+public class Currency
+{
+    public int Id { get; set; }
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public bool IsBase { get; set; }
+    public decimal ConversionRate { get; set; }
+    public DateTime LastModifyOnUtc { get; set; }
+}
---
+++ src/FalconWallet.API/Features/UserWallet/Domain/Wallet.cs
@@ -0,0 +1,14 @@
+﻿using FalconWallet.API.Features.MultiCurrency;
+
+namespace FalconWallet.API.Features.UserWallet.Domain;
+
+public class Wallet
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string? Title { get; set; }
+    public decimal Balance { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public int CurrencyId { get; set; }
+    public Currency Currency { get; set; } = null!;
+}
