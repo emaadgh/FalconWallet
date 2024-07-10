36e9c539d4fe26abe6cd77ad81372e5405be89e0
emaadghorbani <emaadghorbani@gmail.com>
2024-07-10 14:11:30 +0330

    Adds unit tests for Currency domain logics.

---
+++ tests/FalconWallet.UnitTests/Features/MultiCurrency/CurrencyTests.cs
@@ -0,0 +1,42 @@
+﻿using FalconWallet.API.Features.MultiCurrency.Common;
+using FluentAssertions;
+
+namespace FalconWallet.UnitTests.Features.MultiCurrency;
+public class CurrencyTests
+{
+    [Fact]
+    public void Create_ShouldInitializeCurrencyProperly_WhenValidParametersProvided()
+    {
+        // Arrange
+        var name = "US Dollar";
+        var code = "USD";
+        var conversionRate = 1.0m;
+
+        // Act
+        var currency = Currency.Create(name, code, conversionRate);
+
+        // Assert
+        currency.Name.Should().Be(name);
+        currency.Code.Should().Be(code);
+        currency.ConversionRate.Should().Be(conversionRate);
+        currency.LastModifyOnUtc.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+        currency.Wallets.Should().BeNull();
+    }
+
+    [Fact]
+    public void UpdateConversionRate_ShouldUpdateRateAndTimestamp_WhenNewRateIsProvided()
+    {
+        // Arrange
+        var currency = Currency.Create("Euro", "EUR", 0.85m);
+        var newConversionRate = 0.90m;
+        var initialModifyTime = currency.LastModifyOnUtc;
+
+        // Act
+        currency.UpdateConversionRate(newConversionRate);
+
+        // Assert
+        currency.ConversionRate.Should().Be(newConversionRate);
+        currency.LastModifyOnUtc.Should().BeAfter(initialModifyTime);
+        currency.LastModifyOnUtc.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+    }
+}
